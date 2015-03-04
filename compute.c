efc6dd7402d4fb7875d158fda06c89a490073271
George Maratos <ffmpeg1234@gmail.com>
2015-03-04 17:59:58 -0600

    Final commit before push

--- compute.c
+++ compute.c
@@ -1,11 +1,27 @@
 #include "header.h"
+int
+isUsed(wordTable table, wrdPtr wP, tagPtr tP)
+{
+  int i;
+  for (i=0;i<MAXWORD;i++) {
+    if (table[i] != NULL) {
+      if (strcmp(table[i]->word,wP->word) == 0 &&
+  	  strcmp(table[i]->tag,tP->tag) == 0)
+        return 1;
+    }
+  }
+  return 0;
+}
 
 void
 findMLE(wordTable ds, wrdTable wT, tagTable tT)
 {
-  int i,j,k;
+  wordTable array;
+  int i,j,k,l = 0;
   float value;
   if (DEBUG) fprintf(stdout,"findMLE\n");
+  array = malloc(sizeof(wordPtr) * MAXWORD);
+  for (i=0;i<MAXWORD;i++) array[i] = NULL;
   for (i=0;i<MAXWORD;i++) {
     if (wT[i] != NULL) 
       for (j=0;j<MAXWORD;j++) {
@@ -15,7 +31,10 @@ findMLE(wordTable ds, wrdTable wT, tagTable tT)
 	      if (tT[k] != NULL)
 	        if (strcmp(ds[j]->tag,tT[k]->tag) == 0) {
 	          value = calcMLE(wT[i],tT[k],ds);
-	          fprintf(stdout,"Word: %s Tag: %s MLE: %f\n",wT[i]->word,tT[k]->tag,value);
+	          if (isUsed(array,wT[i],tT[k]) == 0) {
+		    fprintf(stdout,"Word: %s Tag: %s MLE: %f\n",wT[i]->word,tT[k]->tag,value);
+		    array[l++] = makeWord(wT[i]->word,tT[k]->tag); 
+		  }
 		}	
 	    }
           }
@@ -36,7 +55,7 @@ float calcMLE(wrdPtr wP, tagPtr tP,wordTable ds)
         top++;
     }
   }
-  fprintf(stdout, "top: %d oc: %d\n",top, tP->occurences);
+  if (DEBUG) fprintf(stdout, "top: %d oc: %d\n",top, tP->occurences);
   value = (float)top / (float)tP->occurences;
   return value;
 }
